1fc51056946b211b7185e4d7be4cfdb6380ad847
olivierdalang <olivier.dalang@gmail.com>
2021-06-25 15:11:26 +0200

    [qwat][export] fix metaattributes

--- qgepqwat2ili/qwat/export.py
+++ qgepqwat2ili/qwat/export.py
@@ -76,7 +76,7 @@ def qwat_export():
             # --- metaattribute ---
             datenherr="unknown",
             datenlieferant="unknown",
-            letzte_aenderung=datetime.datetime.min,
+            letzte_aenderung=datetime.datetime(1970, 1, 1),
             sia405_baseclass_metaattribute=instance.t_id,
             # OD : is this OK ? Don't we need a different t_id from what inserted above in organisation ? if so, consider adding a "for_class" arg to tid_for_row
             t_id=instance.t_id,
@@ -88,11 +88,13 @@ def qwat_export():
         """
         Returns common attributes for base
         """
+        tid = get_tid(row, tid_for_class)
+        oid = f"ch{tid:014}"  # convert to OID
         return {
-            "t_ili_tid": row.id,
+            "t_ili_tid": oid,
             "t_type": type_name,
-            "obj_id": row.id,
-            "t_id": get_tid(row, tid_for_class),
+            "obj_id": oid,
+            "t_id": tid,
         }
 
     def leitungsknoten_common(row):
