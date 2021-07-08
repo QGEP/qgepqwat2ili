8bd11d5ddc9bd455247440e141ccbcad842a6446
olivierdalang <olivier.dalang@gmail.com>
2021-07-08 07:03:19 +0200

    fix #8: don't uncheck BCD damages by default

--- qgepqwat2ili/gui/editors/damage_channel.py
+++ qgepqwat2ili/gui/editors/damage_channel.py
@@ -5,10 +5,14 @@ class DamageChannelEditor(Editor):
 
     class_name = "damage_channel"
 
-    def initially_checked(self):
-        """
-        Determines if the item must be initially checked. To be overriden by subclasses.
-        """
-        if self.obj.channel_damage_code__REL.value_en == "BCD":
-            return False
-        return True
+    # DISABLED FOR NOW AS PER https://github.com/QGEP/qgepqwat2ili/issues/8
+    # this would allow to deselect BCD inspections (start of inspection) by default,
+    # as they don't provide valuable information. Once consensus is reached, we
+    # can remove this Editor altogether.
+    # def initially_checked(self):
+    #     """
+    #     Determines if the item must be initially checked. To be overriden by subclasses.
+    #     """
+    #     if self.obj.channel_damage_code__REL.value_en == "BCD":
+    #         return False
+    #     return True
