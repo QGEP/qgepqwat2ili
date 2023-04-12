b0364bc6657f91f750e33c8d002995a1ab0e0fb4
SJiB <stefan.burckhardt@gmx.ch>
2023-04-12 17:55:01 +0200

    add missing subclass entries for class organisation / add fk_dataowner and provider

--- qgepqwat2ili/data/test_data/qgep_demodata_hotfix.sql
+++ qgepqwat2ili/data/test_data/qgep_demodata_hotfix.sql
@@ -15,3 +15,44 @@ UPDATE qgep_od.reach SET progression_geometry=ST_RemoveRepeatedPoints(progressio
 -- Error: line 84452: SIA405_ABWASSER_2015_LV95.SIA405_Abwasser.Haltung: tid ch13p7mzRE009663: duplicate coord at (2750088.164, 1263080.203, NaN)
 -- degenerated geometry
 DELETE FROM qgep_od.reach WHERE obj_id='ch13p7mzRE009663';
+
+-- add organisations subclass entries to fit requirements for VSA-DSS export
+-- "ch13p7mzOG000002"	"Berg"
+-- "ch13p7mzOG000003"	"Mörschwil"
+-- "ch13p7mzOG000004"	"Tübach"
+-- "ch13p7mzOG000007"	"Arbon"
+-- "ch13p7mzOG000008"	"Frasnacht"
+-- "ch13p7mzOG000009"	"Egnach"
+-- "ch13p7mzOG000010"	"Horn"
+-- "ch13p7mzOG000011"	"Roggwil"
+-- "ch13p7mzOG000012"	"Steinach"
+-- "ch13p7mzOG000013"	"Gemeinde"
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000002');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000003');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000004');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000007');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000008');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000009');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000010');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000011');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000012');
+INSERT INTO qgep_od.municipality (obj_id) VALUES ('ch13p7mzOG000013');
+
+-- "ch13p7mzOG000014"	"Private"
+-- "ch13p7mzOG000006"	"Privat"
+-- "ch13p7mzOG000016"	"unbekannt"
+INSERT INTO qgep_od.private (obj_id) VALUES ('ch13p7mzOG000014');
+INSERT INTO qgep_od.private (obj_id) VALUES ('ch13p7mzOG000006');
+INSERT INTO qgep_od.private (obj_id) VALUES ('ch13p7mzOG000016');
+
+-- "ch13p7mzOG000005"	"AV Morgental"
+-- "ch13p7mzOG000015"	"Verband"
+INSERT INTO qgep_od.waste_water_association (obj_id) VALUES ('ch13p7mzOG000005');
+INSERT INTO qgep_od.waste_water_association (obj_id) VALUES ('ch13p7mzOG000015');
+
+-- also add missing fk_dataowner and fk_provider
+
+UPDATE qgep_od.organisation
+SET fk_dataowner = 'ch13p7mzOG000016',
+    fk_provider = 'ch13p7mzOG000016'
+;
