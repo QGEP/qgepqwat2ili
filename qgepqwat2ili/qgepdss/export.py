a95339612f005f64d05264383e90f65f8ae5aa3b
Damiano Lombardi <damiano@opengis.ch>
2024-09-13 21:45:17 +0200

    debug t_basket value

--- qgepqwat2ili/qgepdss/export.py
+++ qgepqwat2ili/qgepdss/export.py
@@ -185,12 +185,14 @@ def qgep_export(selection=None, labels_file=None, orientation=None):
             t_seq=0,
             t_basket=current_basket.t_id,
         )
+        print(f"create_metaattributes {current_basket.t_id}")
         abwasser_session.add(metaattribute)
 
     def base_common(row, type_name):
         """
         Returns common attributes for base
         """
+        print(f"base_common {current_basket.t_id}")
         return {
             "t_ili_tid": row.obj_id,
             "t_type": type_name,
