beeea8fa14723e7b51e0600a66df335c57a9b2d8
Damiano Lombardi <damiano@opengis.ch>
2024-09-13 15:39:08 +0200

    Upgrade ili tools to 5.0.1

--- qgepqwat2ili/config.py
+++ qgepqwat2ili/config.py
@@ -10,10 +10,7 @@ PGUSER = os.getenv("PGUSER", None)
 PGPASS = os.getenv("PGPASS", None)
 JAVA = r"java"
 
-# 12.7.2022 Anpassung auf 4.61 damit auch VSA-DSS kompatibel siehe https://github.com/claeis/ili2db/issues/374
-# ILI2PG = os.path.join(BASE, "bin", "ili2pg-4.5.0-bindist", "ili2pg-4.5.0.jar")
-ILI2PG = os.path.join(BASE, "bin", "ili2pg-4.6.1-bindist", "ili2pg-4.6.1.jar")
-# ILI2PG = os.path.join(BASE, "bin", "ili2pg-4.8.0-bindist", "ili2pg-4.8.0.jar")
+ILI2PG = os.path.join(BASE, "bin", "ili2pg-5.0.1", "ili2pg-5.0.1.jar")
 ILIVALIDATOR = os.path.join(BASE, "bin", "ilivalidator-1.11.9", "ilivalidator-1.11.9.jar")
 ILI_FOLDER = os.path.join(BASE, "ili")
 DATA_FOLDER = os.path.join(BASE, "data")
