071b65de635981ae4d0b0b65a5c7bc749c185877
SJiB <stefan.burckhardt@gmx.ch>
2024-10-10 11:28:41 +0200

    update qgepdss export.py

--- qgepqwat2ili/qgepdss/export.py
+++ qgepqwat2ili/qgepdss/export.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 
 from .. import utils
+# 4.10.2024
+from ..utils.ili2db import skip_wwtp_structure_ids
 from ..utils.various import logger
 from .model_abwasser import get_abwasser_model
 from .model_qgep import get_qgep_model
@@ -42,6 +44,21 @@ def qgep_export(selection=None, labels_file=None, orientation=None):
 
     # Logging for debugging
     logger.info(f"print subset_ids '{subset_ids}'")
+    
+   # get list of id's of class wwtp_structure (ARABauwerk) to be able to check if fk_wastewater_structure references to wwtp_structure
+                                               
+
+    wastewater_structure_id_sia405abwasser_list = None
+    wastewater_structure_id_sia405abwasser_list = skip_wwtp_structure_ids()
+
+    logger.info(
+        f"wastewater_structure_id_sia405abwasser_list : {wastewater_structure_id_sia405abwasser_list}",
+    )
+
+
+
+
+
 
     # Orientation
     oriented = orientation is not None
