18fa3cee16ac05b47b8df4d8653d97347d831c49
SJiB <stefan.burckhardt@gmx.ch>
2024-11-19 19:38:17 +0100

    correction

--- qgepqwat2ili/qgepdss/export.py
+++ qgepqwat2ili/qgepdss/export.py
@@ -3122,7 +3122,7 @@ def qgep_export(selection=None, labels_file=None, orientation=None):
                 QGEP.water_course_segment,
                 QGEP.measuring_point.fk_water_course_segment == QGEP.water_course_segment.obj_id,
             )
-            .join(QGEP.river, QGEP.water_course_segment.fk_water_course == QGEP.river.obj_id,)
+            .join(QGEP.river, QGEP.water_course_segment.fk_watercourse == QGEP.river.obj_id,)
             .join(QGEP.sector_water_body, QGEP.sector_water_body.fk_surface_water_bodies == QGEP.sector_water_body.obj_id,)
             .join(QGEP.discharge_point, QGEP.measuring_point.fk_sector_water_body == QGEP.discharge_point.obj_id,)
             .join(QGEP.wastewater_networkelement)
