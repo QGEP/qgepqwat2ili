dae92ef031569cd937c4cbb51853b1ac59b0d707
SJiB <stefan.burckhardt@gmx.ch>
2024-12-12 13:04:31 +0100

    take out some logger.debug

--- qgepqwat2ili/utils/qgep_export_utils.py
+++ qgepqwat2ili/utils/qgep_export_utils.py
@@ -875,7 +875,7 @@ def get_connected_we_from_re(subset_reaches):
             strrow = str(row[0])
             if strrow is not None:
                 connected_wn_from_re_ids.append(strrow)
-                logger.debug(f" building up '{connected_wn_from_re_ids}' ...")
+                # logger.debug(f" building up '{connected_wn_from_re_ids}' ...")
     logger.info(f" connected_wn_from_re_ids: '{connected_wn_from_re_ids}'")
     return connected_wn_from_re_ids
 
@@ -922,7 +922,7 @@ def get_connected_overflow_to_wn_ids(selected_ids):
             strrow = str(row[0])
             if strrow is not None:
                 connected_overflow_to_wn_ids.append(strrow)
-                logger.debug(f" building up '{connected_overflow_to_wn_ids}' ...")
+                # logger.debug(f" building up '{connected_overflow_to_wn_ids}' ...")
     logger.info(f" connected_overflow_to_wn_ids: '{connected_overflow_to_wn_ids}'")
     return connected_overflow_to_wn_ids
 
@@ -969,7 +969,7 @@ def get_connected_we_to_re(subset_reaches):
             strrow = str(row[0])
             if strrow is not None:
                 connected_wn_to_re_ids.append(strrow)
-                logger.debug(f" building up '{connected_wn_to_re_ids}' ...")
+                # logger.debug(f" building up '{connected_wn_to_re_ids}' ...")
     logger.info(f" connected_wn_to_re_ids: '{connected_wn_to_re_ids}'")
     return connected_wn_to_re_ids
 
@@ -1106,7 +1106,7 @@ def filter_reaches(selected_ids):
                 strrow = str(row[0])
                 if strrow is not None:
                     all_reaches_ids.append(strrow)
-                    logger.debug(f" building up '{all_reaches_ids}' ...")
+                    # logger.debug(f" building up '{all_reaches_ids}' ...")
 
             for list_item in selected_ids:
                 if list_item in all_reaches_ids:
