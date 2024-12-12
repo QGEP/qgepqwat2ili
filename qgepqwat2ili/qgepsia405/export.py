aacaad25c91ccb45d8194ca0a574626591492ccb
SJiB <stefan.burckhardt@gmx.ch>
2024-12-12 15:45:20 +0100

    check for None

--- qgepqwat2ili/qgepsia405/export.py
+++ qgepqwat2ili/qgepsia405/export.py
@@ -73,34 +73,35 @@ def qgep_export_sia405(selection=None, labels_file=None, orientation=None, baske
         # 6. check blind connections - are there reaches in adapted_subset_ids that have not been in subset_ids
         subset_ids_reaches = filter_reaches(subset_ids)
         adapted_subset_ids_reaches = filter_reaches(adapted_subset_ids)
-        if not adapted_subset_ids_reaches:
-            logger.debug(
-                "no adapted_subset_ids_reaches - so nothing to remove",
-            )
-        else:
-            # https://www.geeksforgeeks.org/python-difference-two-lists/
-            # First convert lists to sets
-            # https://www.w3schools.com/python/ref_set_difference.asp
-            # x = {"apple", "banana", "cherry"}
-            # y = {"google", "microsoft", "apple"}
-            # z = x.difference(y)
-            # replaced with code that first converts to sets
-            # extra_reaches_ids = subset_ids_reaches.difference(adapted_subset_ids_reaches)
-            # Convert lists to sets and use the difference method
-            # c = list(set(a) - set(b))
-            extra_reaches_ids = list(set(subset_ids_reaches) - set(adapted_subset_ids_reaches))
-        # 7. If extra_reaches then remove from adapted_subset_ids
-        if not extra_reaches_ids:
-            # list is empty - no need for adaption
-            logger.debug(
-                "no extra reaches - so nothing to remove from adapted_subset_ids",
-            )
-        else:
-            logger.debug(
-                f"extra_reaches_ids: {extra_reaches_ids} found!",
-            )
-            # if len(extra_reaches_ids) > 0:
-            adapted_subset_ids = remove_from_selection(adapted_subset_ids, extra_reaches_ids)
+        if adapted_subset_ids_reaches is None:
+            if not adapted_subset_ids_reaches:
+                logger.debug(
+                    "no adapted_subset_ids_reaches - so nothing to remove",
+                )
+            else:
+                # https://www.geeksforgeeks.org/python-difference-two-lists/
+                # First convert lists to sets
+                # https://www.w3schools.com/python/ref_set_difference.asp
+                # x = {"apple", "banana", "cherry"}
+                # y = {"google", "microsoft", "apple"}
+                # z = x.difference(y)
+                # replaced with code that first converts to sets
+                # extra_reaches_ids = subset_ids_reaches.difference(adapted_subset_ids_reaches)
+                # Convert lists to sets and use the difference method
+                # c = list(set(a) - set(b))
+                extra_reaches_ids = list(set(subset_ids_reaches) - set(adapted_subset_ids_reaches))
+            # 7. If extra_reaches then remove from adapted_subset_ids
+            if not extra_reaches_ids:
+                # list is empty - no need for adaption
+                logger.debug(
+                    "no extra reaches - so nothing to remove from adapted_subset_ids",
+                )
+            else:
+                logger.debug(
+                    f"extra_reaches_ids: {extra_reaches_ids} found!",
+                )
+                # if len(extra_reaches_ids) > 0:
+                adapted_subset_ids = remove_from_selection(adapted_subset_ids, extra_reaches_ids)
         # 8. get all id's of connected wastewater_structures
         subset_wws_ids = get_ws_selected_ww_networkelements(adapted_subset_ids)
         logger.info(
